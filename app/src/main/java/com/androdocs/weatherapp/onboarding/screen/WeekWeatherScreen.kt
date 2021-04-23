d6e171cc8af97ec6bd1e851644eb57780201c9c5
Rjeey <licanomega@gmail.com>
2021-04-23 13:38:37 +0300

    git commit exeption last testing var

--- app/src/main/java/com/androdocs/weatherapp/onboarding/screen/WeekWeatherScreen.kt
+++ app/src/main/java/com/androdocs/weatherapp/onboarding/screen/WeekWeatherScreen.kt
@@ -1,11 +1,10 @@
 package com.androdocs.weatherapp.onboarding.screen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.Fragment
 import com.androdocs.weatherapp.R
 
 
